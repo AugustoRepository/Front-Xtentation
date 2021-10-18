06458419eced54844d46cbe28369f1566bb32aaf
augusto araujo <augusto.aas@gmail.com>
2021-10-18 15:26:02 -0300

    layoutando as primeiras paginas

---
+++ src/app/login-funcionario/login-funcionario.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginFuncionarioComponent } from './login-funcionario.component';
+
+describe('LoginFuncionarioComponent', () => {
+  let component: LoginFuncionarioComponent;
+  let fixture: ComponentFixture<LoginFuncionarioComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginFuncionarioComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginFuncionarioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
